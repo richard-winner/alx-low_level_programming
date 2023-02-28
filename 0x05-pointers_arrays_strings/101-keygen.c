c1e443ed3c4159ce4236483c1efab60e6d8138d0
dr-Winn <duvorrichardwinner@gmail.com>
2023-02-28 05:34:19 -0800

    Pointers, arrays, strings

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -12,24 +12,23 @@
 int main(void)
 
 {
-	int pass[100];
-	int i, sum, n;
+int pass[100];
+int i, sum, n;
 
-	sum = 0;
-
-	srand(time(NULL));
-	for (i = 0; i < 100; i++)
-	{
-		pass[i] = rand() % 78;
-		sum += (pass[i] + '0');
-		putchar(pass[i] + '0');
-		if ((2772 - sum) - '0' < 78)
-		{
-			n = 2772 - sum - '0';
-			sum += n;
-			putchar(n + '0');
-			break;
-		}
-	return (0);
-	}
+sum = 0;
+srand(time(NULL));
+for (i = 0; i < 100; i++)
+{
+pass[i] = rand() % 78;
+sum += (pass[i] + '0');
+putchar(pass[i] + '0');
+if ((2772 - sum)-'0' < 78)
+{
+n = 2772 - sum - '0';
+sum += n;
+putchar(n + '0');
+break;
+}
+}
+return (0);
 }
