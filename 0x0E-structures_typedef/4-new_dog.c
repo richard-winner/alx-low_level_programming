a74b41b080c0b251be4c3da1d43fe86351c18223
dr-Winn <duvorrichardwinner@gmail.com>
2023-03-20 16:47:12 -0700

    structures, typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,17 +1,15 @@
 #include <stdlib.h>
 #include "dog.h"
-
-/**
- * _strlen - returns the length of a string
- * @s: string to evaluate
- *
- * Return: the length of the string
- */
+/
+* _strlen - returns the length of a string
+* @s: string to evaluate
+*
+* Return: the length of the string
+*/
 
 int _strlen(char *s)
 {
 int i;
-
 i = 0;
 while (s[i] != '\0')
 {
@@ -19,17 +17,18 @@ i++;
 }
 return (i);
 }
-/**
- *_strcpy - copies the string pointed to by src
- * including the terminating null byte (\0)
- * to the buffer pointed to by dest
- * @dest: pointer to the buffer in which we copy the string
- * @src: string to be copied
- *
- * Return: the pointer to dest
- */
 
+/**
+* _strcpy - copies the string pointed to by src
+* including the terminating null byte (\0)
+* to the buffer pointed to by dest
+* @dest: pointer to the buffer in which we copy the string
+* @src: string to be copied
+*
+* Return: the pointer to dest
+*/
 char *_strcpy(char *dest, char *src)
+
 {
 int len, i;
 
@@ -70,6 +69,7 @@ return (NULL);
 }
 dog->owner = malloc(sizeof(char) * (len2 + 1));
 if (dog->owner == NULL)
+{
 free(dog);
 free(dog->name);
 return (NULL);
