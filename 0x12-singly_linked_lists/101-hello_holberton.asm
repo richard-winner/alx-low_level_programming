02807a1ee8b03d622ac256a9f3e7d648cac17b05
dr winner <duvorrichardwinner@gmail.com>
2023-03-31 01:35:27 -0700

    singly linked lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,10 @@
-section .data
-    hello db "Hello, Holberton!", 0
-    format db "%s\n", 0
-
-section .text
-    global main
-    extern printf
-
+global    main
+          extern    printf
 main:
-    push rbp
-    mov rbp, rsp
-
-    mov rdi, format
-    mov rsi, hello
-    xor rax, rax
-    call printf
-
-    mov rsp, rbp
-    pop rbp
-    xor eax, eax
-    ret
+          mov   edi, format
+          xor   eax, eax
+          call  printf
+          mov         eax, 0
+          ret
+format: db 'Hello, Holberton\n',0
+fmt db "%s\n", 0
