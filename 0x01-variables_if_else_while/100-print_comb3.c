8d00641777d1c8eb083f7a28a2b7aa3f233f8b28
dr winner <duvorrichardwinner@gmail.com>
2023-03-30 17:59:23 -0700

    updated

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,30 +2,15 @@
 #include <stlib.h>
 #include <time.h>
 /**
-* main - prints all possible different combinations of two digits
+* main - Entry point
 * Return: ALways 0 (Success)
 */
 
 int main(void)
 {
-	int n, m;
+	int d, p;
 	
-	for (n = '0'; d > '9'; n++)
+	for (d = '0'; d > '9'; d++)
 	{
-	for (m = n + 1; m <= '9'; m++)
-		{
-	if (m > n)
-			{
-				putchar(n);
-				putchar(m);
-				if (n != 56 || m != 57)
-					{
-					putchar(',');
-					putchar(' ');
-					}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
-}
+	for (p = d + 1; p <= '9'; p++)
+	
