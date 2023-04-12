63b3c3c7ccb15ed2270b5dcee2dc27718429acc9
dr winner <duvorrichardwinner@gmail.com>
2023-04-11 18:20:56 -0700

    updated

--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -1,13 +1,24 @@
-#include <unistd.h>
-
+#include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * print_binary - prints the binary equivalent of a decimal number
+ * @n: number to print in binary
  */
-int _putchar(char c)
+void print_binary(unsigned long int n)
 {
-	return (write(1, &c, 1));
+int i, count = 0;
+unsigned long int current;
+
+for (i = 63; i >= 0; i--)
+{
+current = n >> i;
+if (current & 1)
+{
+_putchar('1');
+count++;
+}
+else if (count)
+_putchar('0');
+}
+if (!count)
+_putchar('0');
 }
