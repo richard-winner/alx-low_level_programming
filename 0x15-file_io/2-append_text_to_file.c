bc155c7fde73e5a127ad308221c4e6e0b59352fe
dr winner <duvorrichardwinner@gmail.com>
2023-04-14 22:52:07 -0700

    file io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,14 +13,17 @@ int append_text_to_file(const char *filename, char *text_content)
 int fd;
 int nletters;
 int rwr;
+
 if (!filename)
 return (-1);
+
 fd = open(filename, O_WRONLY | O_APPEND);
 if (fd == -1)
 return (-1);
 if (text_content)
 {
-for (nletters = 0; text_content[nletters]; nletters++);
+for (nletters = 0; text_content[nletters]; nletters++)
+;
 rwr = write(fd, text_content, nletters);
 if (rwr == -1)
 return (-1);
